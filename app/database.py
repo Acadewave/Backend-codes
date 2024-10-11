09b987d7074b76e38a32367a5d26f679361d4dcd
root <root@Favxlaw>
2024-10-11 22:49:45 +0100

    feat: cloud clever for db

--- app/database.py
+++ app/database.py
@@ -2,17 +2,25 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-DATABASE_HOST = os.getenv("DATABASE_HOST")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_PORT = os.getenv("DATABASE_PORT")
+ENVIRONMENT = os.getenv("ENVIRONMENT", "development")
 
-DATABASE_URL = (
+if ENVIRONMENT == "development":
+    DATABASE_USER = os.getenv("DATABASE_USER")
+    DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+    DATABASE_HOST = os.getenv("DATABASE_HOST")  
+    DATABASE_NAME = os.getenv("DATABASE_NAME")
+    DATABASE_PORT = os.getenv("DATABASE_PORT", "3306")
+
+    DATABASE_URL = (
     f"mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
 )
+
+elif ENVIRONMENT == "production":
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
